4201088f253067adbf04919cf9d1d3e92cb3012e
Rahul Kumar <rahul.pathak092@gmail.com>
2021-05-05 18:34:33 +0530

    Part 2: Solution
    Part 2: Solution

--- Restaurant_project -Student version/src/test/java/RestaurantTest.java
+++ Restaurant_project -Student version/src/test/java/RestaurantTest.java
@@ -14,11 +14,23 @@ class RestaurantTest {
     @Test
     public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
         //WRITE UNIT TEST CASE HERE
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        LocalTime currentTime = LocalTime.parse("12:00:00");
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        assertTrue(restaurant.isRestaurantOpen(currentTime));
+
     }
 
     @Test
     public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
-        //WRITE UNIT TEST CASE HERE
+
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+
+        LocalTime currentTime = LocalTime.parse("23:00:00");
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        assertFalse(restaurant.isRestaurantOpen(currentTime));
 
     }
 
